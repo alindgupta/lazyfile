4d68c6b01a5e6828a3e019cf79564bbc43e42e43
Alind Gupta <alindgupta@alinds-mbp.gateway.2wire.net>
2018-02-04 00:57:37 -0500

    documentation and style fixes + removed typing hints

--- lazyfile/utils.py
+++ lazyfile/utils.py
@@ -1,23 +1,39 @@
-""" """
+"""
+utils.py
+~~~~~~~~
+Lazy function application over text.
+"""
 
 import os
 from functools import partial
-from typing import Callable, Any, Generator
 
 
-def fst_arg_last(func: Callable) -> Callable:
-    """ 
-    Returns a new function whose final parameter
+# Note: This function should not be used directly.
+# _fst_arg_last is a helper function for `LazyFile.map_function`
+def _fst_arg_last(func):
+    """ Returns a new function whose final parameter
     is the first parameter of the function arglist
     eg. fst_arg_last will do the following manipulation
     on a function named tokenize
-    
+
+    For example,
+    ```python
     tokenize(string, separator=' ', language='english')
-    -> tokenize(separator=' ', language='english' string)
-    
-    :param func: callable(A,B,C,..)
-    :return: callable(B,C,..,A)
-    
+    ```
+    is transformed into
+    ```python
+    tokenize(separator=' ', language='english', string)
+    ```
+
+    Parameters
+    ----------
+    func: callable(A,B,C,..)
+      A function/callable.
+
+    Returns
+    -------
+    A callable(B,C,..,A)
+
     """
     def inner(*args, **kwargs):
         args = args[1:] + (args[0],)
@@ -25,21 +41,16 @@ def fst_arg_last(func: Callable) -> Callable:
     return inner
 
 
-class LazyFileReader:
-    """
-    
-    Lazily read a text file and apply a function to each line
-    
-    Usage: requires the use of a context-manager so files can be closed
+class LazyFile:
+    """ Lazily read a text file and apply a function to each line
+
+    Usage requires the use of a context-manager so files can be closed
     appropriately
-    
+
     """
-
     def __init__(self, filename: str, buf=-1, enc='utf-8'):
-
         if not os.path.isfile(filename):
-            raise IOError('Could not find', filename)
-
+            raise IOError(f'Could not find {filename}')
         self.context_managed = False
         self._file = filename           # filename
         self._fhandle = None            # file handle
@@ -47,52 +58,71 @@ class LazyFileReader:
         self.buf = buf
         self.enc = enc
 
+    # context manager - enter clause
     def __enter__(self):
         self._fhandle = open(self._file, 'r', self.buf, self.enc)
         self.lines = (line for line in self._fhandle)
         self.context_managed = True
         return self
 
+    # context manager - exit clause
     def __exit__(self, exec_type, exec_value, traceback):
         if exec_type is not None:
             print(exec_type, exec_value, traceback)
         if self._fhandle:
             self._fhandle.close()
         else:
-            raise Exception('Improperly closed fhandle!')
+            raise Exception('Improperly closed file!')
 
-    def apply(self, func, *args, **kwargs) -> Generator:
-        """
-        Apply function to lines from file
-        
-        :param func: Function that operates on string
-        :param args: Optional arguments to func
-        :param kwargs: Optional keyword arguments to func
-        :return: Generator
+    @property
+    def file(self):
+        return self._file
+
+    def apply(self, func, *args, **kwargs):
+        """ Apply function to lines from file
+
+        Parameters
+        ----------
+        func: Callable[[str], Any]
+          A function that operates on strings.
+
+        Optional args and kwargs for func.
+
+        Returns
+        -------
+        A generator.
+
         """
         if not self.context_managed:
             raise Exception('Object must be initialized with context manager')
         try:
             yield from (func(line, *args, **kwargs) for line in self.lines)
-        except TypeError as e:
-            print(e)
-        except Exception as e:
-            print(e)
+        except Exception:
+            raise
 
-    def mapfunc(self, func: Callable[[str], Any], *args, **kwargs) -> Generator:
+    def map_function(self, func, *args, **kwargs):
+        """ Another (lazier) version of apply using Python's `map` function
+
+        Parameters
+        ----------
+        func: Callable[[str], Any]
+          A function that operates on strings.
+
+        Optional args and kwargs for func.
+
+        Returns
+        -------
+        A generator.
+
         """
-        Lazier version of apply using Python's map function
-
-         :param func: Function that operates on string
-         Use partialfunc to generate a partial function easily
-         :return: Generator
-         """
         if not self.context_managed:
             raise Exception('Object must be initialized with context manager')
         try:
-            yield from map(func if not args else partial(fst_arg_last(func), *args, **kwargs),
-                           self.lines)
-        except TypeError as e:
-            print(e)
-        except Exception as e:
-            print(e)
+            yield from map(
+                func if not args else partial(
+                    _fst_arg_last(func),
+                    *args,
+                    **kwargs),
+                self.lines)
+        except Exception:
+            raise
