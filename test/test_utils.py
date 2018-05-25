0c01cf914b0fb1f64069ec71b5ae864ee4da4ec2
Alind Gupta <alindgupta19@gmail.com>
2018-05-25 15:01:07 -0400

    Add tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,32 @@
 import lazyfile
+from lazyfile import utils
 
 def test_initialization():
+    # no argument
+    try:
+        a = utils.Lazyfile()
+        assert False
+    except TypeError:
+        assert True
+    
+    # no such file
+    try:
+        a = utils.Lazyfile('')
+        assert False
+    except OSError:
+        assert True
+
+    # no context manager
+    try:
+        a = utils.Lazyfile('text.txt')
+        a.map_function(len)
+        a.apply(len)
+        assert False
+    except:
+        assert True
+
+    # correct use
+    with utils.Lazyfile('text.txt') as txt:
+        print(txt.map_function(len))
     assert True
+        
